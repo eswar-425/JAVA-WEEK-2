2c3a3253d9c6e372f44adf3a720a005739c1978b
ESWAR YASWANTH KUMAR KODA <eswaryaswanthkumarkoda7@gmail.com>
2024-08-27 16:05:11 +0530

    Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -3,14 +3,8 @@ class Cons{
 Cons(int a){
 System.out.println("Value of a is: "+a)
 }
-Cons(int a,int b)
-{
- System.out.println("Sum of two numbers is: "+(a + b));
-}
 public static void main(String args[]){
 Cons obj = new Cons(10);
-Cons obj1 = new Cons(10,20);
 }
 }
 Output : Value of a is: 10
-         Sum of two numbers is: 30
